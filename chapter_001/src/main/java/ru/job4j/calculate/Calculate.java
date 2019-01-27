b5400d23540917086a7a3cda8eca6e551da30b03
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-27 21:31:56 +0300

    1.3. JavaDoc. Code convention[#109037]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,31 @@
 package ru.job4j.calculate;
 
+/**
+ *Calculate.
+ *
+ *@author Krylov Sergey (krylov.sergey.1999@yandex.ru)
+ */
+
+
 public class Calculate {
-    public static void main(String[] args) {
-        System.out.println("Hello  World!");
-    }
+    	/**
+	 * Main.
+	 * @param args - args.
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	/**
+	 * Method echo.
+	 * @param value
+	 * @return value = ...
+	 */
+	public final String echo(final String value) {
+		if (value != null) {
+			value = String.format("%s value = ", value);
+		} else {
+			value = "value = null";
+		}
+		return String.format("%s %s %s", value, value, value);
+	}
 }
