2d7dee014d25851e68fa9ca63e7c15cbfd9349a6
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-28 16:52:23 +0300

    3.4. Расстояние между точками в системе координат[#109029]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author Крылов Сергей
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+        );
+    }
+}
