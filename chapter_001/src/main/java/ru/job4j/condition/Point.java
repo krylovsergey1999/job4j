95e230391a852da34bc887abdbf4cb83d0f3eef4
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-28 17:06:44 +0300

    Добавил доки к точке.

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 /**
+ * Класс точка.
  * @author Крылов Сергей
  * @version $Id$
  * @since 0.1
@@ -10,11 +11,22 @@ public class Point {
     private int x;
     private int y;
 
+    /**
+     * Конструктор точки.
+     * @param x - ось X
+     * @param y - ось Y
+     */
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Метод находит расстояние между точками, той от который мы метод вызываем и той которую передаем.
+     * Используем просто теорему пифагора.
+     * @param that - точка
+     * @return - расстояние
+     */
     public double distanceTo(Point that) {
         return Math.sqrt(
                 Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
