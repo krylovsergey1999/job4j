d45ce101817c3a49539acbfe4a58f3a915f3c57b
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-29 00:09:33 +0300

    6.6. Двухмерный массив. Таблица умножения.[#109016]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Matrix {
+    /**
+     * Двухмерный массив. Таблица умножения
+     *
+     * @param size
+     * @return
+     */
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                table[i - 1][j - 1] = i * j;
+            }
+        }
+        return table;
+    }
+}
