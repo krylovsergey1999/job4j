242635816d4cf0f3e9bb32befacd26fbd581d893
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-28 17:56:01 +0300

    4.2. Максимум из двух чисел[#109001]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max;
+
+public class Max {
+    /**
+     * Максимум из двух элементов использовал тернарный оператор.
+     * @param first - число
+     * @param second - число
+     * @return - максимум из 2 чисел
+     */
+    public int max(int first, int second) {
+        return first>second ? first : second;
+    }
+
+}
