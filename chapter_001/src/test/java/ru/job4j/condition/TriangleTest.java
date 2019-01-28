49eb02759a84f7222f1411f0e2e61a7fe2c21210
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-28 18:18:26 +0300

    4.3. Вычисление площади треугольника[#109004]

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+/**
+ * @author Krylov Sergey
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class TriangleTest {
+
+    /**
+     * Тест когда треугольник можно создать.
+     */
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // создаем три объекта класса Point.
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+        // Создаем объект треугольник и передаем в него объекты точек.
+        Triangle triangle = new Triangle(a, b, c);
+        // Вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+
+    /**
+     * Все три точки на одной прямой, треугольник невозможен.
+     */
+    @Test
+    public void whenTriangleImpossible() {
+        // создаем три объекта класса Point.
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 1);
+        Point c = new Point(0, 2);
+        // Создаем объект треугольник и передаем в него объекты точек.
+        Triangle triangle = new Triangle(a, b, c);
+        // Вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = -1D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
