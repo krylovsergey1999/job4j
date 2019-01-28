4cf8cc95ff5fc3ebf390e61764081270f647fcab
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-28 17:01:13 +0300

    Конвертер добавил тесты и доки

--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ConverterTest {
+    /**
+     *  Рубли в доллары конвертит и проверяет.
+     */
     @Test
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
@@ -12,10 +15,33 @@ public class ConverterTest {
         assertThat(result, is(1));
     }
 
+    /**
+     *  Рубли в евро конвертит и проверяет.
+     */
     @Test
     public void when70RubleToEuroThen1() {
         Converter converter = new Converter();
         int result = converter.rubleToEuro(70);
         assertThat(result, is(1));
+    }
+
+    /**
+     *  Евро в рубли конвертит и проверяет.
+     */
+    @Test
+    public void whenEuro70ToRuble() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(70);
+        assertThat(result, is(4900));
+    }
+
+    /**
+     *  Доллары в рубли конвертит и проверяет.
+     */
+    @Test
+    public void whenDollar60ToRuble() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(50);
+        assertThat(result, is(3000));
     }
 }
