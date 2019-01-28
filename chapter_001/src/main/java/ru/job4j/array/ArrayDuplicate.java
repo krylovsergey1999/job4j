b1f7287b752d56e6c456a654d55c68c0343611be
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-29 00:39:16 +0300

    6.8. Удаление дубликатов в массиве.[#109021]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+    /**
+     * 6.8. Удаление дубликатов в массиве.[#109021]
+     *
+     * @param array массив
+     * @return массив без дубликатов
+     */
+    public String[] remove(String[] array) {
+        int l = array.length;
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                if (array[i].equals(array[j])) {
+                    for (int h = j; h < l - 1; h++)
+                        array[h] = array[h + 1];
+                    l--;
+                }
+            }
+        }
+        return Arrays.copyOf(array, l);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+
+    @Test
+    public void whenSevenEl() {
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] input = {"1", "2", "3", "4", "5", "1", "6", "2", "7"};
+        String[] expected = {"1", "2", "3", "4", "5", "6", "7"};
+        String[] result = duplicate.remove(input);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenFiveEl() {
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] expected = {"Привет", "Мир", "Супер"};
+        String[] result = duplicate.remove(input);
+        assertThat(result, is(expected));
+    }
+}
