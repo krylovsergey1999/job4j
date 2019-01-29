398e4756676b602f1513ef06f3274320f5270b3f
Krylov Sergey <krylov.sergey.1999@yandex.ru>
2019-01-29 11:13:55 +0300

    Добавил puzzle

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -14,8 +14,9 @@ public class ArrayDuplicate {
         for (int i = 0; i < l; i++) {
             for (int j = i + 1; j < l; j++) {
                 if (array[i].equals(array[j])) {
-                    for (int h = j; h < l - 1; h++)
+                    for (int h = j; h < l - 1; h++) {
                         array[h] = array[h + 1];
+                    }
                     l--;
                     j--;
                 }
